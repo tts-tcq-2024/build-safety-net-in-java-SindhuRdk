24113c3159edf2c1e86608b5d5ff0669ac04452e
SindhuRdk <52288683+SindhuRdk@users.noreply.github.com>
2024-09-06 10:39:21 +0530

    Update SoundexTest.java

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -1,6 +1,6 @@
 package CodeTestCoverJava;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -8,11 +8,48 @@ public class SoundexTest {
 
     @Test
     public void testEmptyString() {
-       //  assertTrue(Soundex.generateSoundex("")=="");
+        assertEquals("", Soundex.generateSoundex(""));
+    }
+
+    @Test
+    public void testNullString() {
+        assertEquals("", Soundex.generateSoundex(null));
     }
 
     @Test
     public void testSingleCharacter() {
-         //assertTrue(Soundex.generateSoundex("A")=="A000");
+        assertEquals("A000", Soundex.generateSoundex("A"));
+    }
+
+    @Test
+    public void testNoConsonants() {
+        assertEquals("A000", Soundex.generateSoundex("AEIOU"));
+    }
+
+    @Test
+    public void testConsonantsOnly() {
+        assertEquals("S532", Soundex.generateSoundex("Smith"));
+        assertEquals("R163", Soundex.generateSoundex("Robert"));
+    }
+
+    @Test
+    public void testRepeatedConsonants() {
+        assertEquals("T520", Soundex.generateSoundex("Ttfn"));
+    }
+
+    @Test
+    public void testPadding() {
+        assertEquals("W252", Soundex.generateSoundex("Wright"));
+        assertEquals("P200", Soundex.generateSoundex("Park"));
+    }
+
+    @Test
+    public void testMixedCase() {
+        assertEquals("R163", Soundex.generateSoundex("RoBeRt"));
+    }
+
+    @Test
+    public void testHAndWHandling() {
+        assertEquals("R300", Soundex.generateSoundex("RWH"));
     }
 }
