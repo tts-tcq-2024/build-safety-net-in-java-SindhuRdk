77cb4ba2c3d8c7222bc96157d93ee3726737855d
SindhuRdk <52288683+SindhuRdk@users.noreply.github.com>
2024-09-06 12:00:40 +0530

    Update SoundexTest.java

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -28,28 +28,28 @@ public class SoundexTest {
 
     @Test
     public void testConsonantsOnly() {
-        assertEquals("S532", Soundex.generateSoundex("Smith"));
-        assertEquals("R163", Soundex.generateSoundex("Robert"));
+        assertEquals("S532", Soundex.generateSoundex("Smith")); // Expected S532
+        assertEquals("R163", Soundex.generateSoundex("Robert")); // Expected R163
     }
 
     @Test
     public void testRepeatedConsonants() {
-        assertEquals("T520", Soundex.generateSoundex("Ttfn"));
+        assertEquals("T520", Soundex.generateSoundex("Ttfn")); // Expected T520
     }
 
     @Test
     public void testPadding() {
-        assertEquals("W252", Soundex.generateSoundex("Wright"));
-        assertEquals("P200", Soundex.generateSoundex("Park"));
+        assertEquals("W252", Soundex.generateSoundex("Wright")); // Expected W252
+        assertEquals("P200", Soundex.generateSoundex("Park")); // Expected P200
     }
 
     @Test
     public void testMixedCase() {
-        assertEquals("R163", Soundex.generateSoundex("RoBeRt"));
+        assertEquals("R163", Soundex.generateSoundex("RoBeRt")); // Expected R163
     }
 
     @Test
     public void testHAndWHandling() {
-        assertEquals("R300", Soundex.generateSoundex("RWH"));
+        assertEquals("R300", Soundex.generateSoundex("RWH")); // Expected R300
     }
 }
