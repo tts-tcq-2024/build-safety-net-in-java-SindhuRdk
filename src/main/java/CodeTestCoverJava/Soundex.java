d95e65763b69dfc15dea525b7989fe441e363c5a
SindhuRdk <52288683+SindhuRdk@users.noreply.github.com>
2024-09-06 10:38:57 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -9,7 +9,7 @@ public class Soundex {
 
         StringBuilder soundex = new StringBuilder();
         soundex.append(Character.toUpperCase(name.charAt(0)));
-        char prevCode = getSoundexCode(name.charAt(0));
+        char prevCode = '0'; // Initialize with '0' for no code
 
         for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
             char code = getSoundexCode(name.charAt(i));
@@ -19,30 +19,25 @@ public class Soundex {
             }
         }
 
-        while (soundex.length() < 4) {
-            soundex.append('0');
-        }
-
-        return soundex.toString();
+        return padSoundex(soundex);
     }
 
     private static char getSoundexCode(char c) {
-        c = Character.toUpperCase(c);
-        switch (c) {
-            case 'B': case 'F': case 'P': case 'V':
-                return '1';
-            case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z':
-                return '2';
-            case 'D': case 'T':
-                return '3';
-            case 'L':
-                return '4';
-            case 'M': case 'N':
-                return '5';
-            case 'R':
-                return '6';
-            default:
-                return '0'; // For A, E, I, O, U, H, W, Y
+        switch (Character.toUpperCase(c)) {
+            case 'B': case 'F': case 'P': case 'V': return '1';
+            case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
+            case 'D': case 'T': return '3';
+            case 'L': return '4';
+            case 'M': case 'N': return '5';
+            case 'R': return '6';
+            default: return '0'; // For vowels and ignored characters
         }
+    }
+
+    private static String padSoundex(StringBuilder soundex) {
+        while (soundex.length() < 4) {
+            soundex.append('0');
+        }
+        return soundex.toString();
     }
 }
